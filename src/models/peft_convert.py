2638a419af020e4915f4d1c4f4a9ed3ee8cf3392
Markel <git@markel.dev>
2024-02-22 20:13:30 +0100

    Refactor conversion to other file

--- src/models/peft_convert.py
+++ src/models/peft_convert.py
@@ -13,8 +13,38 @@ from peft.mapping import get_peft_model
 from peft.peft_model import PeftModel
 from peft.tuners.lora.config import LoraConfig
 from transformers import T5Model
+from ..utils.arguments import Args
 
 logger = logging.getLogger("m.models.peft_convert")
+
+def convert_to_peft(model: T5Model, args: Args) -> PeftModel:
+    """
+    Converts the given T5 model to a PeftModel based on the specified method.
+
+    Args:
+        model (T5Model): The T5 model to be converted.
+        args (Args): The inline arguments of the program.
+
+    Returns:
+        PeftModel: The converted PeftModel.
+
+    Raises:
+        ValueError: If the args are not correctly passed.
+    """
+    if args.method == "LoRA":
+        logger.debug("Method selected: LoRA. Proceeding to convert the model.")
+        if (isinstance(args.rank, type(None)) or isinstance(args.alpha, type(None)) or
+            isinstance(args.dropout, type(None)) or isinstance(args.target_modules, type(None))):
+            logger.critical("Method LoRA requires rank, alpha, dropout and target modules. \
+                            Parser should have failed.")
+            raise ValueError("Method LoRA requires rank, alpha, dropout and target modules.")
+        
+        peft_model = convert_to_lora(model,
+                                     args.rank, args.alpha, args.dropout, args.target_modules)
+    else:
+        logger.critical("Method %s not recognized, parser should have failed.", args.method)
+        raise ValueError("Method not recognized.")
+    return peft_model
 
 def convert_to_lora(model: T5Model,
                     rank: int, alpha: int,
--- src/peft_analyze.py
+++ src/peft_analyze.py
@@ -2,7 +2,7 @@
 
 from .utils.torchfuncs import get_device
 from .utils.logger import set_logger_config_and_return
-from .models.peft_convert import convert_to_lora
+from .models.peft_convert import convert_to_peft
 
 from .models.orig_model import download_model
 from .utils.arguments import Args, parse_args
@@ -17,16 +17,5 @@ def main() -> None:
     logger.debug("Arguments: %s", ARGS)
     logger.info("Device: %s", DEVICE)
     model, tokenizer = download_model(ARGS.model, DEVICE) # pylint: disable=unused-variable
-
-    #* Get PEFT-ize version of the model
-    if ARGS.method == "LoRA":
-        logger.debug("Method selected: LoRA. Proceeding to convert the model.")
-        if (isinstance(ARGS.rank, type(None)) or isinstance(ARGS.alpha, type(None)) or
-            isinstance(ARGS.dropout, type(None)) or isinstance(ARGS.target_modules, type(None))):
-            logger.critical("Method LoRA requires rank, alpha, dropout and target modules. \
-                            Parser should have failed.")
-            return
-        model = convert_to_lora(model, ARGS.rank, ARGS.alpha, ARGS.dropout, ARGS.target_modules)
-    else:
-        logger.critical("Method %s not recognized, parser should have failed.", ARGS.method)
-        return
+    model = convert_to_peft(model, ARGS)
+    
