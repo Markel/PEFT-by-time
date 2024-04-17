4ef3bf5896228648a2ebf0d48d948dcd883737cb
Markel <git@markel.dev>
2024-04-17 13:04:27 +0200

    Include evaluation metrics and results conversion

--- src/dataset/ag_news.py
+++ src/dataset/ag_news.py
@@ -9,8 +9,8 @@ from datasets import DatasetDict, load_dataset
 from torch import Tensor, device
 import torch
 from torchmetrics import MetricCollection
-from torchmetrics.classification import (BinaryAccuracy, BinaryF1Score,
-                                         BinaryPrecision, BinaryRecall)
+from torchmetrics.classification import (MulticlassAccuracy, MulticlassF1Score,
+                                         MulticlassPrecision, MulticlassRecall)
 from transformers import T5TokenizerFast
 
 from .base_dataset import BaseDataset
@@ -46,10 +46,17 @@ class AGNews(BaseDataset):
         self.tokenized_results.append(tokenizer.encode("The topic of the article is: Business",
                                       padding="max_length", max_length=len(longest)))
         self.tokenized_results.append(longest)
-      
+
         dataset_dict = dataset_dict.map(
             lambda x: {"token_labels": self.tokenized_results[x["label"]]}
         )
+
+        # pylint: disable=consider-using-enumerate
+        for i in range(len(self.tokenized_results)):
+            while self.tokenized_results[i][-1] == 0:
+                self.tokenized_results[i] = self.tokenized_results[i][:-1]
+        # pylint: enable=consider-using-enumerate
+
         dataset_dict = dataset_dict.rename_column("label", "labels")
         dataset_dict = dataset_dict.map(
             lambda x: tokenizer(x["text"], max_length=441, padding="max_length", truncation=True,
@@ -57,7 +64,6 @@ class AGNews(BaseDataset):
         )
         logger.debug("DatasetDict converted successfully.")
 
-        print(dataset_dict)
         train_and_dev = dataset_dict["train"].train_test_split(test_size=dev_size, seed=42)
         self.train = train_and_dev["train"].with_format("torch", device=device_t)
         self.dev = train_and_dev["test"].with_format("torch", device=device_t)
@@ -72,17 +78,37 @@ class AGNews(BaseDataset):
         This method will return an evaluate a combination that contain the corresponding
         metrics to evaluate the dataset.
         """
-        raise NotImplementedError
+        metric_collection = MetricCollection([
+            MulticlassAccuracy(num_classes=4, average="macro"),
+            MulticlassPrecision(num_classes=4, average="macro"),
+            MulticlassRecall(num_classes=4, average="macro"),
+            MulticlassF1Score(num_classes=4, average="macro")
+        ]).to(device_t)
+        return metric_collection
 
     def pre_eval_func(self, batch) -> Tensor:
         """
         This method will return the tensor to be used for evaluation given the outputs
         of the model.
 
-        Negative answers are considered for all cases where it doens't match the
-        positive label. However, when computing the loss, these are independent classes.
+        When none of the phrases are found, the output is considered to be the first class.
         """
-        raise NotImplementedError
+        output_label = batch.logits.argmax(-1)
+        output_label = output_label.tolist()
+        def get_label(x):
+            if x[:len(self.tokenized_results[0])] == self.tokenized_results[0]:
+                return 0
+            elif x[:len(self.tokenized_results[1])] == self.tokenized_results[1]:
+                return 1
+            elif x[:len(self.tokenized_results[2])] == self.tokenized_results[2]:
+                return 2
+            elif x[:len(self.tokenized_results[3])] == self.tokenized_results[3]:
+                return 3
+            else:
+                return 0
+        output_label = [get_label(x) for x in output_label]
+        output_label = torch.tensor(output_label).to(batch.logits.get_device())
+        return output_label
 
 if __name__ == "__main__":
     logger.critical("Module not meant to be run as a script. Exiting.")
